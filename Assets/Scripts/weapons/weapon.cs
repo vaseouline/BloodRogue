633a8b51dc51120ede503d656944b54a3373106e
Min Young Park <mypark283@gmail.com>
2021-04-11 18:42:40 -0400

    Added utlities script

--- Assets/Scripts/weapons/weapon.cs
+++ Assets/Scripts/weapons/weapon.cs
@@ -50,20 +50,7 @@ public abstract class weapon : MonoBehaviour {
         shootInterval = 0;
         swingInterval = 0;
         automatic = false;
-        //At the moment, entity doesn't have handposition and it shouldn't
-        //because bullet is also an entity.
-        //Just checking for now if equiper has Player or Enemy script and check for handposition from there.
-        if(equipper.GetComponent<Player>() != null) {
-            Debug.Log("Set parent object");
-            this.transform.parent = equipper.GetComponent<Player>().handPosition.transform;
-        }
-        //Right now enemy doesn't have handPosition as well
-        /*
-        else if (equiper.GetComponent<enemy>() != null) {
-            this.transform.parent = equiper.GetComponent<enemy>().handPosition.transform;
-        }
-        */
-        
+
     }
 
     protected void Update() {
@@ -75,6 +62,7 @@ public abstract class weapon : MonoBehaviour {
         swingInterval--;
 
     }
+
 
     public abstract bool Shoot(Vector2 heldPosition);
 
